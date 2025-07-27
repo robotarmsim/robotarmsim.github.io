ba90b57ef0dc1c61824fe5f643c680b640c21989
dmprgm_omen <dmprgm@gmail.com>
2025-07-27 19:52:49 -0400

    Deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-// src/main.tsx
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './css/index.css'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './css/index.css'; // Assuming Tailwind is set up here
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
